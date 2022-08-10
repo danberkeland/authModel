58ede285a2231e4125fe7b4d33b245024c9650ee
DanBerkeland <danberkeland@gmail.com>
2022-08-10 06:40:59 -0700

    Dan was here

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ export function App() {
 
   return (
     <React.Fragment>
-      <h1>Kato was Here</h1>
+      <h1>Dan was Here</h1>
       Welcome {userDetails.userName}. Location: {userDetails.locName}. Authtype:{" "}
       {authType}.
       {formType === "signedIn" && (
